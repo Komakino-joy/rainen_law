76ba7fa16b837f2342258b9a05a88583c9362ae7
Bryan Alvarez <bryan.alvarez1251@gmail.com>
2023-03-14 21:01:09 -0700

    added printing option for property report

--- src/pages/reports/property-report/PropertyReportPage.tsx
+++ src/pages/reports/property-report/PropertyReportPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { useForm } from "react-hook-form";
 import FormInput from '@/components/Forms/Common/FormInput/FormInput';
 import Button from '@/components/Button/Button';
@@ -10,6 +10,7 @@ import ReportProperties from '@/components/Tables/ReportProperties/ReportPropert
 import InfoCard from '@/components/InfoCard/InfoCard';
 import RegistryOfDeedsBreakDown from '@/components/RegistryOfDeedsBreakDown/RegistryOfDeedsBreakDown';
 import TitlesBreakDown from '@/components/TitleBreakDown/TitlesBreakDown';
+import { useReactToPrint } from 'react-to-print';
 
 export default function PropertyReport() {
   
@@ -63,6 +64,15 @@ export default function PropertyReport() {
     reset()
   };
 
+  const componentRef = useRef(null);
+  const handlePrint = useReactToPrint({
+    content: () => componentRef.current,
+  });
+
+  const getPageMargins = () => {
+    return `@page { margin: 10px 20px !important; }`;
+  };
+
   return (
     <div className={`flex-y gap-md ${styles['property-page-content'] }`}>
       <div className='form-wrapper is-date-selection card-shadow light-border center-margin'>
@@ -96,7 +106,17 @@ export default function PropertyReport() {
         </form>
       </div>
       { propertiesData && propertiesData.length > 0 ?
-          <div className={styles['property-report-wrapper']}>
+        <div className={styles['property-report-wrapper']} >
+          <style>{getPageMargins()}</style>
+            <Button 
+              onClick={handlePrint} 
+              isDisabled={false} 
+              type={'button'}
+              customClass={styles['print-report-button']}
+            >
+              Print Report
+            </Button>
+          <div ref={componentRef} className={styles['property-report-printable-area']}>
             <header>
               <p>Property Report</p>
               <span>{timestampToDate(reportRunDate.start, 'mmDDyyyy').date} - {timestampToDate(reportRunDate.end, 'mmDDyyyy').date}</span>
@@ -115,6 +135,7 @@ export default function PropertyReport() {
               countyCountMap={countyCountMap} 
             />
           </div>
+        </div>
         : 
         <div className={styles['info-section']}>
           <InfoCard line1='Select a date range' line2='to generate report' />
